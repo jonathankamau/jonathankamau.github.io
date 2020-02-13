ec1b028d162eaf2f1fda892375700735474365de
Jonathan Kamau <jonathan.kamau@andela.com>
2020-02-13 14:13:18 +0300

    [content] add functionality to read content from json files

---
+++ scripts/load-json.js
@@ -0,0 +1,60 @@
+var jsonContent = new Array();
+    $.ajax({
+      url: "../data/",
+      success: function(data){
+         $(data).find("a").each(function(){
+            if(openFile($(this).attr("href"))){
+                var fileName = $(this).attr("href")
+                $.getJSON( "data/"+fileName, function( data ) {
+                    jsonContent.push(data)
+                    $.each(data, function(key, val){
+                        populateData(key, val);
+                        
+                    })
+                    
+                })
+            }        
+         });
+         
+      }
+      
+    }); 
+function openFile(file) {
+    var extension = file.substr( (file.lastIndexOf('.') +1) );
+    switch(extension) {
+        case 'json':
+            return true;
+            break;
+        default:
+            return false;
+    }
+};
+
+function populateData(key, val) {
+    if (document.getElementById(key)) {
+        var key = document.getElementById(key)
+        var showChar = 200;
+        var ellipsestext = "...";
+        var moretext = "Read More";
+        
+        if(val.length > showChar) {
+            var c = val.substr(0, showChar);
+            var h = val.substr(showChar, val.length);
+    
+            var newVal = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+            key.innerHTML = newVal;
+            
+        }
+        else {
+            key.innerHTML = val;
+        }
+    }
+    
+    
+
+    
+    
+}
+
+
+
